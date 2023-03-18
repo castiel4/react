53aa1bed8afa86778a9ff6f2c9deff83e98e05bb
castiel4 <119147575+castiel4@users.noreply.github.com>
2023-03-18 16:17:14 -0300

    Add files via upload

---
+++ 3.1/App.js
@@ -0,0 +1,18 @@
+importar  './App.css' ;
+importar  dados  de  './components/Dados' ;
+
+
+função  Aplicativo ( )  {
+
+   nome const = 'Davi'
+   idade const = '17 anos'
+  const  curso = 'front-end'
+  
+  retornar  (
+  < >
+    < Dados  nome = { nome }  idade = { idade }  curso = { curso }  / >
+  < / >
+  )
+}
+
+exportar  aplicativo padrão  ;
---
+++ 3.1/componentes/dados.js
@@ -0,0 +1,11 @@
+função  Dados  ( props )  {
+    retornar  (
+        < seção >
+            < p > Nome: { adereços . nome } </p >
+            < p > Idade: { props . idade } </ p >
+            < p > Curso: { props . cursor } </p >
+        < / seção >
+    )
+}
+
+exportar  dados padrão  ;
